337239930a436eb8a52d7f2bdb17d2e8e61e95a0
Frank Ong <frankongh@gmail.com>
2022-10-16 23:20:13 -0700

    Add band boundaries

--- slfrank/plot.py
+++ slfrank/plot.py
@@ -1,80 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sigpy.mri.rf as rf
 from . import transform
-
-
-def plot_pulse(pulse, m=1000, ptype='ex', phase='linear',
-               omega_range=[-np.pi, np.pi],
-               linewidth=2, fontsize='x-large', labelsize='large'):
-    figs = []
-    n = len(pulse)
-    fig, ax = plt.subplots()
-    ax.plot(pulse.real, label=r'$B_{1, \mathrm{x}}$', linewidth=linewidth)
-    ax.plot(pulse.imag, label=r'$B_{1, \mathrm{y}}$', linewidth=linewidth)
-    ax.set_title(r'$B_1$ (Energy={0:.3g}, Peak={1:.3g})'.format(
-        np.sum(np.abs(pulse)**2), np.max(np.abs(pulse))), fontsize=fontsize)
-    ax.set_xlabel('Time', fontsize=fontsize)
-    ax.legend(fontsize=fontsize)
-    ax.yaxis.set_tick_params(labelsize=labelsize)
-    ax.xaxis.set_tick_params(labelsize=labelsize)
-    figs.append(fig)
-
-    omega = np.linspace(omega_range[0], omega_range[1], m)
-    psi_z = np.exp(-1j * np.outer(omega, np.arange(n)))
-    a, b = transform.forward_slr(pulse)
-    alpha = psi_z @ a
-    beta = psi_z @ b
-
-    if ptype == 'se':
-        m_xy = beta**2
-        m_xy *= np.exp(1j * omega * (n - 1))
-        fig, ax = plt.subplots()
-        ax.set_title(r'$M_{\mathrm{xy}}$')
-        ax.set_xlabel(r'$\omega$ [radian]')
-        ax.plot(omega, np.real(m_xy), label=r'$M_{\mathrm{x}}$', linewidth=linewidth)
-        ax.plot(omega, np.imag(m_xy), label=r'$M_{\mathrm{y}}$', linewidth=linewidth)
-        ax.legend(fontsize=fontsize)
-        ax.yaxis.set_tick_params(labelsize=labelsize)
-        ax.xaxis.set_tick_params(labelsize=labelsize)
-        figs.append(fig)
-    else:
-        m_xy = 2 * alpha.conjugate() * beta
-        m_z = np.abs(alpha)**2 - np.abs(beta)**2
-        if phase == 'linear':
-            m_xy *= np.exp(1j * omega * n / 2)
-
-        fig, ax = plt.subplots()
-        ax.set_title(r'$|M_{\mathrm{xy}}|$', fontsize=fontsize)
-        ax.set_xlabel(r'$\omega$ [radian]', fontsize=fontsize)
-        ax.plot(omega, np.abs(m_xy), linewidth=linewidth)
-        ax.yaxis.set_tick_params(labelsize=labelsize)
-        ax.xaxis.set_tick_params(labelsize=labelsize)
-        figs.append(fig)
-
-        fig, ax = plt.subplots()
-        ax.set_title(r'$\angle M_{\mathrm{xy}}$', fontsize=fontsize)
-        ax.set_xlabel(r'$\omega$ [radian]', fontsize=fontsize)
-        ax.plot(omega, np.angle(m_xy), linewidth=linewidth)
-        ax.yaxis.set_tick_params(labelsize=labelsize)
-        ax.xaxis.set_tick_params(labelsize=labelsize)
-        figs.append(fig)
-
-        fig, ax = plt.subplots()
-        ax.set_title(r'$M_{\mathrm{z}}$', fontsize=fontsize)
-        ax.set_xlabel(r'$\omega$ [radian]', fontsize=fontsize)
-        ax.plot(omega, m_z, linewidth=linewidth)
-        ax.yaxis.set_tick_params(labelsize=labelsize)
-        ax.xaxis.set_tick_params(labelsize=labelsize)
-        figs.append(fig)
-
-    return figs
+from . import design
 
 
 def plot_slr_pulses(pulse_slr, pulse_slfrank,
                     m=1000, ptype='ex', phase='linear',
                     omega_range=[-np.pi, np.pi],
+                    tb=4, d1=0.01, d2=0.01,
                     fontsize='x-large', labelsize='large'):
     n = len(pulse_slr)
+    dinf = rf.dinf(d1, d2)
+    w = dinf / tb
+    bands = [[max(-np.pi, omega_range[0]), -(1 + w) * tb / n * np.pi],
+             [-(1 - w) * tb / n * np.pi, (1 - w) * tb / n * np.pi],
+             [(1 + w) * tb / n * np.pi, min(np.pi, omega_range[1])]]
+    boundaries = [-(1 + w) * tb / n * np.pi,
+             -(1 - w) * tb / n * np.pi,
+             (1 - w) * tb / n * np.pi,
+             (1 + w) * tb / n * np.pi,
+             ]
 
     fig, axs = plt.subplots(2, 2)
     axs[0][0].plot(pulse_slr.real,
@@ -110,6 +56,13 @@ def plot_slr_pulses(pulse_slr, pulse_slfrank,
 
         axs[1][0].set_title(r'$M_{\mathrm{x}}$')
         axs[1][0].set_xlabel(r'$\omega$ [radian]')
+        axs[1][0].vlines(x=boundaries,
+                         ymin=min(np.amin(np.real(m_xy_slr)), np.amin(np.real(m_xy_slfrank))),
+                         ymax=max(np.amax(np.real(m_xy_slr)), np.amax(np.real(m_xy_slfrank))),
+                         colors='gray',
+                         linestyle='dotted',
+                         linewidth=0.5,
+                         label='Band Boundaries')
         axs[1][0].plot(omega, np.real(m_xy_slr),
                        linewidth=0.5,
                        label=r'SLR',
@@ -121,6 +74,13 @@ def plot_slr_pulses(pulse_slr, pulse_slfrank,
 
         axs[1][1].set_title(r'$M_{\mathrm{y}}$')
         axs[1][1].set_xlabel(r'$\omega$ [radian]')
+        axs[1][1].vlines(x=boundaries,
+                         ymin=min(np.amin(np.imag(m_xy_slr)), np.amin(np.imag(m_xy_slfrank))),
+                         ymax=max(np.amax(np.imag(m_xy_slr)), np.amax(np.imag(m_xy_slfrank))),
+                         colors='gray',
+                         linestyle='dotted',
+                         linewidth=0.5,
+                         label='Band Boundaries')
         axs[1][1].plot(omega, np.imag(m_xy_slr),
                        linewidth=0.5,
                        label=r'SLR',
@@ -132,6 +92,13 @@ def plot_slr_pulses(pulse_slr, pulse_slfrank,
 
         axs[0][1].set_title(r'$M_{\mathrm{z}}$')
         axs[0][1].set_xlabel(r'$\omega$ [radian]')
+        axs[0][1].vlines(x=boundaries,
+                         ymin=min(np.amin(m_z_slr), np.amin(m_z_slfrank)),
+                         ymax=max(np.amax(m_z_slr), np.amax(m_z_slfrank)),
+                         colors='gray',
+                         linestyle='dotted',
+                         linewidth=0.5,
+                         label='Band Boundaries')
         axs[0][1].plot(omega, m_z_slr,
                        linewidth=0.5,
                        label=r'SLR',
@@ -151,6 +118,13 @@ def plot_slr_pulses(pulse_slr, pulse_slfrank,
 
         axs[1][0].set_title(r'$|M_{\mathrm{xy}}|$')
         axs[1][0].set_xlabel(r'$\omega$ [radian]')
+        axs[1][0].vlines(x=boundaries,
+                         ymin=min(np.amin(np.abs(m_xy_slr)), np.amin(np.abs(m_xy_slfrank))),
+                         ymax=max(np.amax(np.abs(m_xy_slr)), np.amax(np.abs(m_xy_slfrank))),
+                         colors='gray',
+                         linestyle='dotted',
+                         linewidth=0.5,
+                         label='Band Boundaries')
         axs[1][0].plot(omega, np.abs(m_xy_slr),
                        linewidth=0.5,
                        label=r'SLR',
@@ -173,6 +147,13 @@ def plot_slr_pulses(pulse_slr, pulse_slfrank,
 
         axs[0][1].set_title(r'$M_{\mathrm{z}}$')
         axs[0][1].set_xlabel(r'$\omega$ [radian]')
+        axs[0][1].vlines(x=boundaries,
+                         ymin=min(np.amin(m_z_slr), np.amin(m_z_slfrank)),
+                         ymax=max(np.amax(m_z_slr), np.amax(m_z_slfrank)),
+                         colors='gray',
+                         linestyle='dotted',
+                         linewidth=0.5,
+                         label='Band Boundaries')
         axs[0][1].plot(omega, m_z_slr,
                        linewidth=0.5,
                        label=r'SLR',
